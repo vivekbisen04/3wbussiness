8c104c028c93674e42f67b46cd81c34b4ad0856a
vivekbisen04 <121126140+vivekbisen04@users.noreply.github.com>
2024-10-12 00:23:15 +0530

    Admin Buttoned centered

--- frontend/src/Componets/Home.jsx
+++ frontend/src/Componets/Home.jsx
@@ -11,18 +11,18 @@ const Home = () => {
 
   return (
     <div className="flex justify-center flex-col">
-      <div className="flex items-center align-middle pt-20">
+      <div className="flex items-center flex-col align-middle pt-20">
         <UserForm />
-      </div>
-      <div className="pt-10">
-        <button
-          className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
+        <div className="pt-10">
+          <button
+            className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
             border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
             active:border-b-[2px] active:brightness-90 active:translate-y-[2px] w-40"
-          onClick={() => navigate("/admin/login")}
-        >
-          Admin
-        </button>
+            onClick={() => navigate("/admin/login")}
+          >
+            Admin
+          </button>
+        </div>
       </div>
     </div>
   );
